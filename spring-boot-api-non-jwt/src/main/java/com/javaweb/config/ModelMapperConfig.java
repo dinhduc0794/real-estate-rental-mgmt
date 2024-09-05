89267300fa657cd1aff2841fccc227f2626d3a5a
dinhduc0794 <ngdduc79@gmail.com>
2024-09-05 19:33:18 +0700

    Edit ModelMapperConfig

--- spring-boot-api-non-jwt/src/main/java/com/javaweb/config/ModelMapperConfig.java
+++ spring-boot-api-non-jwt/src/main/java/com/javaweb/config/ModelMapperConfig.java
@@ -11,8 +11,6 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         // Tạo object và cấu hình
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
     }
 }
