9fbc921ad21fe4d6fca56295f2dc11a3ffa45807
dinhduc0794 <ngdduc79@gmail.com>
2024-09-25 15:20:25 +0700

    Update converter to jpa

--- spring-boot-api-non-jwt/src/main/java/com/javaweb/converter/BuildingConverter.java
+++ spring-boot-api-non-jwt/src/main/java/com/javaweb/converter/BuildingConverter.java
@@ -17,56 +17,17 @@ import com.javaweb.repository.entity.RentAreaEntity;
 @Component
 public class BuildingConverter {
 	@Autowired
-	private DistrictRepository districtRepository;
-	@Autowired
-	private RentAreaRepository rentAreaRepository;
-	@Autowired
 	private ModelMapper modelMapper;
 
-	public BuildingResponseDTO toBuildingResponseDTO(BuildingEntity buildingEntity) {
-		BuildingResponseDTO buildingResponseDTO = modelMapper.map(buildingEntity, BuildingResponseDTO.class);	//2 tham so: source va destination class
-		//name district
-//		DistrictEntity districtEntity = districtRepository.findById(buildingEntity.getDistrictId());
+	public BuildingResponseDTO toBuildingResponseDTO(BuildingEntity et) {
+		BuildingResponseDTO buildingResponseDTO = modelMapper.map(et, BuildingResponseDTO.class);	//2 tham so: source va destination class
 		
-		//string values rentarea
-		List<RentAreaEntity> rentAreaEntities = rentAreaRepository.findByBuildingId(buildingEntity.getId());
-		String rentAreaValues = rentAreaValueToString(rentAreaEntities);
+		buildingResponseDTO.setAddress(et.getStreet() + ", " + et.getWard() + ", " + et.getDistrict().getName());		
 		
-		// buildingResponseDTO.setName(buildingEntity.getName());
-//		buildingResponseDTO.setAddress(buildingEntity.getStreet() + ", " + buildingEntity.getWard() + ", " + districtEntity.getName());
-		// buildingResponseDTO.setNumberOfBasement(buildingEntity.getNumberOfBasement());
-		// buildingResponseDTO.setManagerName(buildingEntity.getManagerName());
-		// buildingResponseDTO.setManagerPhone(buildingEntity.getManagerPhone());
-		// buildingResponseDTO.setFloorArea(buildingEntity.getFloorArea());
-		buildingResponseDTO.setVacantArea(null);
-		buildingResponseDTO.setRentArea(rentAreaValues);
-		// buildingResponseDTO.setRentPrice(buildingEntity.getRentPrice());	
-		// buildingResponseDTO.setServiceFee(buildingEntity.getServiceFee());
-		// buildingResponseDTO.setBrokerageFee(buildingEntity.getBrokerageFee());
-
+		String rentArea = et.getRentAreaEntities().stream()
+											.map(i -> i.getValue().toString()).collect(Collectors.joining(", "));
+		buildingResponseDTO.setRentArea(rentArea);
+		
 		return buildingResponseDTO;
 	}
-	
-	public String rentAreaValueToString(List<RentAreaEntity> rentAreaEntities) {
-		return rentAreaEntities.stream()
-                .map(it -> it.getValue().toString())
-                .collect(Collectors.joining(", "));
-	}
-
-//	public static String rentAreaValueToString(List<RentAreaEntity> raEntities) {
-//		StringBuilder rentAreaValues = new StringBuilder();
-//        for (RentAreaEntity it : raEntities) {
-//            rentAreaValues.append(it.getValue()).append(", ");
-//        }
-//
-//        String result = rentAreaValues.toString();
-//        
-//        //bo dau phay va space o cuoi
-//        if (result.length() > 0) {
-//            result = result.substring(0, result.length() - 2); 
-//        }
-//
-//        return result;
-//	}	
 }
-	
