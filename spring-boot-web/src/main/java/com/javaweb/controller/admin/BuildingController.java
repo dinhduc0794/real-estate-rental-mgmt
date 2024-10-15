046787cf4ab2756c9d37d8ffd1ee4f30dddfaf17
dinhduc0794 <ngdduc79@gmail.com>
2024-10-15 18:19:39 +0700

    First commit

---
+++ spring-boot-web/src/main/java/com/javaweb/controller/admin/BuildingController.java
@@ -0,0 +1,12 @@
+package com.javaweb.controller.admin;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value="buildingControllerOfAdmin")
+public class BuildingController {
+
+
+}
